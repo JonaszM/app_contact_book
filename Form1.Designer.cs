bb6d1914cff39f959afadce1f7831b60d679c22d
Jonasz <Jonasz@DESKTOP-NJFT8BM>
2023-05-27 20:42:07 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ksiązka_kucharska
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblImie = new System.Windows.Forms.Label();
+            this.tboxName = new System.Windows.Forms.TextBox();
+            this.tboxSurname = new System.Windows.Forms.TextBox();
+            this.lbSurname = new System.Windows.Forms.Label();
+            this.tboxPhone = new System.Windows.Forms.TextBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Title.Location = new System.Drawing.Point(13, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(276, 25);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "KSIĄŻKA TELEFONICZNA";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(401, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(378, 381);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(17, 410);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(179, 410);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(98, 410);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Atualizuj";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(260, 410);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Wyczyść";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lblImie
+            // 
+            this.lblImie.AutoSize = true;
+            this.lblImie.Location = new System.Drawing.Point(42, 111);
+            this.lblImie.Name = "lblImie";
+            this.lblImie.Size = new System.Drawing.Size(26, 13);
+            this.lblImie.TabIndex = 6;
+            this.lblImie.Text = "Imię";
+            // 
+            // tboxName
+            // 
+            this.tboxName.Location = new System.Drawing.Point(98, 108);
+            this.tboxName.Name = "tboxName";
+            this.tboxName.Size = new System.Drawing.Size(161, 20);
+            this.tboxName.TabIndex = 7;
+            // 
+            // tboxSurname
+            // 
+            this.tboxSurname.Location = new System.Drawing.Point(98, 149);
+            this.tboxSurname.Name = "tboxSurname";
+            this.tboxSurname.Size = new System.Drawing.Size(161, 20);
+            this.tboxSurname.TabIndex = 9;
+            // 
+            // lbSurname
+            // 
+            this.lbSurname.AutoSize = true;
+            this.lbSurname.Location = new System.Drawing.Point(42, 152);
+            this.lbSurname.Name = "lbSurname";
+            this.lbSurname.Size = new System.Drawing.Size(53, 13);
+            this.lbSurname.TabIndex = 8;
+            this.lbSurname.Text = "Nazwisko";
+            // 
+            // tboxPhone
+            // 
+            this.tboxPhone.Location = new System.Drawing.Point(98, 191);
+            this.tboxPhone.Name = "tboxPhone";
+            this.tboxPhone.Size = new System.Drawing.Size(161, 20);
+            this.tboxPhone.TabIndex = 11;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(42, 194);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(43, 13);
+            this.lbName.TabIndex = 10;
+            this.lbName.Text = "Telefon";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "by Jonasz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tboxPhone);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.tboxSurname);
+            this.Controls.Add(this.lbSurname);
+            this.Controls.Add(this.tboxName);
+            this.Controls.Add(this.lblImie);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "Książka telefoniczna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblImie;
+        private System.Windows.Forms.TextBox tboxName;
+        private System.Windows.Forms.TextBox tboxSurname;
+        private System.Windows.Forms.Label lbSurname;
+        private System.Windows.Forms.TextBox tboxPhone;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ksiązka_kucharska
+{
+    public partial class Form1 : Form
+    {
+        private List<Kontakt> kontakty;
+        public Form1()
+        {
+            kontakty = new List<Kontakt>();
+            InitializeComponent();
+        }
+
+        private void ClearForm()
+        {
+            tboxName.Text = "";
+            tboxSurname.Text = "";
+            tboxPhone.Text = "";
+        }
+
+        private void UpdateContactList()
+        {
+            listBox1.Items.Clear();
+            foreach (Kontakt kontakt in kontakty)
+            {
+                listBox1.Items.Add(kontakt);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = tboxName.Text;
+            string surname = tboxSurname.Text;
+            string phoneNumber = tboxPhone.Text;
+            
+
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(surname) && !string.IsNullOrEmpty(phoneNumber))
+            {
+                if (!IsPhoneNumberValid(phoneNumber))
+                {
+                    MessageBox.Show("Zły format numeru telefonicznego!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else 
+                {
+                    Kontakt kontakt = new Kontakt(name, surname, phoneNumber);
+                    kontakty.Add(kontakt);
+                    ClearForm();
+                    UpdateContactList();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wypełnij wszystkie pola!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int indeks = listBox1.SelectedIndex;
+            if (indeks >= 0)
+            {
+                string name = tboxName.Text;
+                string surname = tboxSurname.Text;
+                string phoneNumber = tboxPhone.Text;
+
+                if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(surname) && !string.IsNullOrEmpty(phoneNumber))
+                {
+                    if (!IsPhoneNumberValid(phoneNumber)) 
+                    {
+                        MessageBox.Show("Zły format numeru telefonicznego!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        kontakty[indeks].Name = name;
+                        kontakty[indeks].Surname = surname;
+                        kontakty[indeks].PhoneNumber = phoneNumber;
+                        ClearForm();
+                        UpdateContactList();
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Wypełnij wszystkie pola!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wybierz kontakt do edycji!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int indeks = listBox1.SelectedIndex;
+            if (indeks >= 0)
+            {
+                kontakty.RemoveAt(indeks);
+                ClearForm();
+                UpdateContactList();
+            }
+            else
+            {
+                MessageBox.Show("Wybierz kontakt do usunięcia!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indeks = listBox1.SelectedIndex;
+            if (indeks >= 0)
+            {
+                Kontakt kontakt = kontakty[indeks];
+                tboxName.Text = kontakt.Name;
+                tboxSurname.Text = kontakt.Surname;
+                tboxPhone.Text = kontakt.PhoneNumber;
+            }
+        }
+
+        private bool IsPhoneNumberValid(string number)
+        {
+            if(number.Length != 9) 
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    char charNumber = (char)number[i];
+                    if (char.IsDigit(charNumber)) continue;
+                    else return false;
+                }
+                return true;
+            }
+            
+        }
+    }
+    public class Kontakt
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Kontakt(string imie, string surname, string phoneNumber)
+        {
+            Name = imie;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname} - {PhoneNumber}";
+        }
+    }
+
+}
